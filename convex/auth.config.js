0a32ff1f27f9946ae91c62869755f1a01d4a2f3c
Ethan Jacinto <ethankylejacinto@gmail.com>
2024-02-10 12:06:01 -0800

    feat: configured convex && clerk authentication

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://polished-barnacle-58.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button size="sm">Click me</Button>;
+  return (
+    <div className="flex fkex-col gap-y-4">
+      This is a screen for authenticated users only
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
